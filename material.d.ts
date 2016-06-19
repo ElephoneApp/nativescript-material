616e0849e5d1cc837341d49ee1b0823bf10dd40f
Bob Singor <bobsingor@Bobs-MacBook-Pro.local>
2016-06-19 19:23:39 +0300

    Add files

---
+++ material.d.ts
@@ -0,0 +1,9 @@
+declare module "nativescript-material" {
+    import { View } from "ui/core/view";
+    import { Property } from "ui/core/dependency-observable";
+    import { Image } from "ui/image";
+    import { Color } from "color";
+    import { EventData } from "data/observable";
+
+
+}
---
+++ material.ios.ts
@@ -0,0 +1,9 @@
+import {ContentView} from 'ui/content-view';
+import {Color} from "color";
+import * as uiUtils from 'ui/utils';
+import {PropertyMetadata} from 'ui/core/proxy';
+import {Property, PropertyMetadataSettings} from 'ui/core/dependency-observable';
+
+export class Button extends ContentView {
+
+}
